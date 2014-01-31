376ecd044de0e1c528b4c628741883f8dbf181c0
Stephen Kelly <stephen.kelly@kdab.com>
2014-01-31 12:49:25 +0100

    Add sphinx configuration and build files.

---
+++ conf.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Patent Defense documentation build configuration file, created by
+# sphinx-quickstart on Fri Jan 31 12:36:22 2014.
+
+import sys, os
+
+# -- General configuration -----------------------------------------------------
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = u'Patent Defense'
+copyright = u'2014, Stephen Kelly'
+
+version = '1.0'
+release = '1.0'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for HTML output ---------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'default'
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'PatentDefensedoc'
---
+++ index.rst
@@ -0,0 +1,21 @@
+.. Patent Defense documentation master file, created by
+   sphinx-quickstart on Fri Jan 31 12:36:22 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Qt Patent Defense
+=================
+
+Defensive Patent Publications for Qt:
+
+.. toctree::
+  :maxdepth: 1
+
+  Type-Erased Container Processing <type-erased-container-processing/type-erased-container-iteration>
+
+
+Other
+=====
+
+ * `Qt Assistance Documentation <PatentDefense.qch>`_
+ * :ref:`search`
